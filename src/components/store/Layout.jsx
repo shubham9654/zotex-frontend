9f69160d7b2d0c591bf7078c312556f1f048120c
Shubham <shubhamsarkar9654@gmail.com>
2024-06-14 17:47:10 +0530

    redndering image

--- src/components/store/Layout.jsx
+++ src/components/store/Layout.jsx
@@ -1,6 +1,5 @@
 
 import { Outlet } from "react-router-dom";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -10,25 +9,6 @@ const Layout = () => {
 
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
-          <script src="/js/vendor/jquery.js"></script>
-          <script src="/js/vendor/waypoints.js"></script>
-          <script src="/js/bootstrap-bundle.js"></script>
-          <script src="/js/swiper-bundle.js"></script>
-          <script src="/js/slick.js"></script>
-          <script src="/js/range-slider.js"></script>
-          <script src="/js/magnific-popup.js"></script>
-          <script src="/js/nice-select.js"></script>
-          <script src="/js/purecounter.js"></script>
-          <script src="/js/wow.js"></script>
-          <script src="/js/isotope-pkgd.js"></script>
-          <script src="/js/jarallax.js"></script>
-          <script src="/js/imagesloaded-pkgd.js"></script>
-          <script src="/js/ajax-form.js"></script>
-          <script src="/js/main.js"></script>
-        </Helmet>
-      </HelmetProvider>
       {/* <Loader /> */}
       <Header />
       <Outlet />
